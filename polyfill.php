6679b9a40881b02344ee521132a9e3257e1fe041
chles <hello@chles.me>
2020-04-29 16:01:38 +0200

    Polyfilll php

---
+++ polyfill.php
@@ -0,0 +1,11 @@
+<?php
+
+// Because array_key_first need PHP 7 >= 7.3.0
+if (!function_exists('array_key_first')) {
+    function array_key_first(array $arr) {
+        foreach($arr as $key => $unused) {
+            return $key;
+        }
+        return NULL;
+    }
+}
